2607dde39d62c2cd2ff09d069d7658714998145d
Eagle Dev <eagledev00.00@gmail.com>
2024-10-14 05:44:04 +0900

    Update make.bat
    Update make.bat
    
    Co-authored-by: Bunny <michaelbunny425@gmail.com>

--- docs/make.bat
+++ docs/make.bat
@@ -20,6 +20,11 @@ if errorlevel 9009 (
 	echo.to the full path of the 'sphinx-build' executable. Alternatively you
 	echo.may add the Sphinx directory to PATH.
 	echo.
+	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+	echo.installed, then set the SPHINXBUILD environment variable to point
+	echo.to the full path of the 'sphinx-build' executable. Alternatively you
+	echo.may add the Sphinx directory to PATH.
+	echo.
 	echo.If you don't have Sphinx installed, grab it from
 	echo.http://sphinx-doc.org/
 	exit /b 1
